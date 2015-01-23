13e2be38fd08ce7867d83ba270380c2b0a1c341b
D34THWINGS <d34thwings@gmail.com>
2015-01-23 17:02:39 +0100

    Botstrap angular with yeoman

--- src/LinkR/linkr-angular/app/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('linkR', [
-  'ngRoute'
-]).config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
---
+++ src/LinkR/linkr-angular/src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('linkR')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
